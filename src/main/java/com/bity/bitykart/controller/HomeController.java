035ed339d4890ab7caf5e4b0cb674e9a84b4aac1
Biswarup <biswarupmaity2004official@gmail.com>
2025-11-10 10:41:29 +0530

    postgre added

--- src/main/java/com/bity/bitykart/controller/HomeController.java
+++ src/main/java/com/bity/bitykart/controller/HomeController.java
@@ -2,11 +2,13 @@ package com.bity.bitykart.controller;
 
 import com.bity.bitykart.dto.OrderDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/test")
 public class HomeController {
     @GetMapping
     public String Home(){
